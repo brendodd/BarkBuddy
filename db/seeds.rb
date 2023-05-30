d41982fe7bef39c88a84c198a8372d44d90ea44e
Davide Cifali <davide.cifali@hotmail.it>
2023-05-30 18:03:34 +0200

    dog show attirbutes

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+Dog.destroy_all
+User.destroy_all
+ben = User.create!(first_name: "Ben", last_name: "Pham", email: "ben@lewagon.com", password: "secret")
+tom = User.create!(first_name: "Tom", last_name: "Thompson", email: "tom@lewagon.com", password: "secret")
+rupert = Dog.create!(name: "Rupert", birthday: "2022-03-12", size: "Large", breed: "Labrador", address: "Harlem", user: ben)
+binny = Dog.create!(name: "Binny", birthday: "2019-04-15", size: "Small", breed: "Bichon", address: "Amsterdam-West", user: tom)
