c36607200d85e1aceac5fc921a9c406687e34420
Raman Jeganathan <raman.jeg@gmail.com>
2023-05-31 15:48:44 +0200

    dogIndex with dog cards

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,5 +1,13 @@
 class DogsController < ApplicationController
+
   def index
     @dogs = Dog.all
+
+    @markers = @dogs.geocoded.map do |dog|
+      {
+        lat: dog.latitude,
+        lng: dog.longitude
+      }
+    end
   end
 end
