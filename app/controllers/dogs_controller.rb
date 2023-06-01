e368ef7da9cd2949efff2ab0d8c69d6ee5c16fa7
Brendan Dodd <brendananthonydodd@gmail.com>
2023-06-01 15:58:14 +0200

    add-listing

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -14,4 +14,24 @@ class DogsController < ApplicationController
     @dog = Dog.find(params[:id])
     @booking = Booking.new
   end
+
+  def new
+    @dog = Dog.new
+  end
+
+  def create
+    @dog = Dog.new(dog_params)
+    @dog.user = current_user
+    if @dog.save
+      redirect_to dog_path(@dog)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def dog_params
+    params.require(:dog).permit(:name, :birthday, :size, :breed, :address)
+  end
 end
